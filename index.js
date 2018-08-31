496a99621ac891af5e4d552da67889dcf4bbc16d
Mund Vetter <mundvetter@gmail.com>
2018-09-01 01:10:44 +0200

    made endscreen

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ const game = new Game()
 game.paddles[0].controls = [{key: 'w', action: 'up'}, {key: 's', action: 'down'}]
 game.paddles[1].controls = [{key: 'up', action: 'up'}, {key: 'down', action: 'down'}]
 const controller = new Controller(game)
-// console.log(game.paddles[1].controls)
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
@@ -42,10 +41,15 @@ process.stdin.on('keypress', (str, key) => {
 
 ctx.hideCursor();
 setInterval(() => {
-  // console.log(controller.getController())
   game.update(controller.getController())
 
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  if(game.ended) {
+    console.log(` ${game.winner[0]} is the winner!`)
+    process.exit()
+  }
+
+  ctx.fillText(`${game.paddles[0].points} - ${game.paddles[1].points}`, canvas.width / 2, 1)
 
   for (let i = 0; i < game.paddles.length; i++) {
     const paddle = game.paddles[i]
@@ -53,19 +57,16 @@ setInterval(() => {
     const size = mp(...paddle.size, canvas, game)
     const pos = mp(...paddle.pos, canvas, game)
     ctx.fillStyle = 'white'
-    ctx.fillText(paddle.points + '', pos[0], pos[1] + 10)
     ctx.fillRect(pos[0], pos[1], size[0], size[1])
   }
   const ball = game.ball
   const bpos = mp(...ball.pos, canvas, game)
-  const bsize = mp(ball.size, ball.size, canvas, game)
+  let bsize = mp(ball.size, ball.size, canvas, game)
+  bsize = (bsize[0] < 1 || bsize[1] < 1) ? [1, 1] : bsize
+  // console.log(bsize)
   ctx.fillStyle = 'white'
   ctx.fillRect(bpos[0], bpos[1], bsize[0], bsize[1])
 }, 1000 / 60);
-
-// function tenth(num) {
-//   return num / 10
-// }
 
 function mp (x, y, canvas, game) {
   return new Uint8Array([x * (canvas.width / 640), y * (canvas.height / 360)])
